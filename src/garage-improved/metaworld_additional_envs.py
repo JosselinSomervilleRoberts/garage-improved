241ae6498fba0191862897807b4e2deb119b2db3
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-05-24 23:23:11 -0700

    Add MT3 and MTFlexible

---
+++ src/garage-improved/metaworld_additional_envs.py
@@ -0,0 +1,79 @@
+import metaworld
+import metaworld.envs.mujoco.env_dict as _env_dict
+
+from collections import OrderedDict
+
+from metaworld.envs.mujoco.sawyer_xyz.v2 import (
+    SawyerPickPlaceEnvV2,
+    SawyerPushEnvV2,
+    SawyerReachEnvV2,
+    SawyerDoorEnvV2,
+    SawyerDrawerOpenEnvV2,
+    SawyerDrawerCloseEnvV2,
+    SawyerButtonPressTopdownEnvV2,
+    SawyerPegInsertionSideEnvV2,
+    SawyerWindowOpenEnvV2,
+    SawyerWindowCloseEnvV2,
+)
+
+# =========== Custom MT3 (3 first tasks of MT10) =========== #
+
+MT3_V2 = OrderedDict(
+    (('reach-v2', SawyerReachEnvV2),
+     ('push-v2', SawyerPushEnvV2),
+     ('pick-place-v2', SawyerPickPlaceEnvV2) )
+)
+
+MT3_V2_ARGS_KWARGS = {
+    key: dict(args=[],
+              kwargs={'task_id': list(_env_dict.ALL_V2_ENVIRONMENTS.keys()).index(key)})
+    for key, _ in MT3_V2.items()
+}
+
+class MT3(metaworld.Benchmark):
+    def __init__(self, seed=None):
+        super().__init__()
+        self._train_classes = MT3_V2
+        self._test_classes = OrderedDict()
+        train_kwargs = MT3_V2_ARGS_KWARGS
+        self._train_tasks = metaworld._make_tasks(self._train_classes, train_kwargs,
+                                        metaworld._MT_OVERRIDE,
+                                        seed=seed)
+        self._test_tasks = []
+
+# ========================================================= #
+
+
+
+# =========== Custom MTFlexible (N first tasks of MT10) =========== #
+
+# Ordered by increasing difficulty (according to the paper)
+MTFLEXIBLE_V2 = OrderedDict(
+    (('reach-v2', SawyerReachEnvV2),
+     ('drawer-close-v2', SawyerDrawerCloseEnvV2),
+     ('drawer-open-v2', SawyerDrawerOpenEnvV2),
+     ('window-open-v2', SawyerWindowOpenEnvV2),
+     ('button-press-topdown-v2', SawyerButtonPressTopdownEnvV2),
+     ('window-close-v2', SawyerWindowCloseEnvV2),
+     ('door-open-v2', SawyerDoorEnvV2),
+     ('push-v2', SawyerPushEnvV2),
+     ('peg-insert-side-v2', SawyerPegInsertionSideEnvV2),
+     ('pick-place-v2', SawyerPickPlaceEnvV2)),)
+     
+class MTFlexible(metaworld.Benchmark):
+    def __init__(self, n: int, seed=None):
+        super().__init__()
+        assert n >= 2, "n must be >= 2, otherwise it is not multi-task"
+        assert n <= 10, "n must be <= 10, as it currently uses MT10"
+        # Keep the first n tasks of MTN_V2
+        self._train_classes = OrderedDict(list(MTFLEXIBLE_V2.items())[:n])
+        self._test_classes = OrderedDict()
+        train_kwargs = {key: dict(args=[],
+                                    kwargs={'task_id': list(_env_dict.ALL_V2_ENVIRONMENTS.keys()).index(key)})
+                        for key, _ in self._train_classes.items()}
+        self._train_tasks = metaworld._make_tasks(self._train_classes, train_kwargs,
+                                        metaworld._MT_OVERRIDE,
+                                        seed=seed)
+        self._test_tasks = [] 
+
+# ========================================================= #
